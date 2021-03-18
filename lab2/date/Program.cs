4187879e9fc192ff434d29e534f2addf71ab7504
Ilya Lazuk <36516154+LaRtik@users.noreply.github.com>
2021-03-18 22:42:23 +0300

    Create Program.cs

---
+++ lab2/date/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Lab2
+ * Prints the names of months in selected language
+*/
+using System;
+using System.Globalization;
+
+namespace Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // to delete ? in output
+            CultureInfo language = null;
+            while (language == null)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the language (ru - Russian, fr - French, be - Belarusian and so on..):");
+                    Console.WriteLine("(if the language doesn't exists, English language will be selected or input ERROR occured)");
+                    language = CultureInfo.GetCultureInfo(Console.ReadLine());
+                }
+                catch (CultureNotFoundException)
+                {
+                    Console.WriteLine("An input error has occured. Please try again.");
+                }
+            }
+            CultureInfo.CurrentCulture = language;
+
+            DateTime date = new DateTime();
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(date.AddMonths(i).ToString("MMMM"));
+            }
+        }
+    }
+}
